f9516b791cf039bb527a4e48508ef19fbb4f46f3
Hunter W <hjwebb9@gmail.com>
2025-11-22 19:30:55 -0500

    refactor everything, basically complete wipe

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { TransactionsProvider } from "./context/TransactionsContext";
-import { BudgetsProvider } from "./context/BudgetsContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <TransactionsProvider>
-        <BudgetsProvider>
-          <App />
-        </BudgetsProvider>
-      </TransactionsProvider>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
